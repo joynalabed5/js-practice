f4ee9923f15db59a3310461ddcdddf5861e564be
joynalabed5 <engr.joynal.bd@gmail.com>
2026-09-09 22:54:49 +0600

    # Feature   - more function

---
+++ function/simple-func.js
@@ -0,0 +1,10 @@
+//named function
+const hello = function hello() {
+    console.log("Hello World");
+}
+hello(); // Output: Hello World
+
+const ello = (joy, rahim) => {
+    console.log(`Hello ${joy} and ${rahim}`);
+}
+ello("Joy", "Rahim"); // Output: Hello Joy and Rahim
