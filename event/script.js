b8575ae4514f75c7b4f385dffd402c1a883e81f5
joynalabed5 <engr.joynal.bd@gmail.com>
2026-09-11 23:39:49 +0600

    #Feature   update

--- event/script.js
+++ event/script.js
@@ -1,3 +1,10 @@
- function handleClick(){
-    alert('Button clicked!');
+//  function handleClick(){
+//     alert('Button clicked!');
+// }
+
+function hello() {
+    console.log("Hello World");
 }
+
+document.getElementById('myButton').addEventListener('click', hello);
+
