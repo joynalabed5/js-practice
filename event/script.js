a95729c12ed526c777464e3ddea8739ed00dea1d
joynalabed5 <engr.joynal.bd@gmail.com>
2026-09-12 23:47:47 +0600

    # feature   -- event and function with click

--- event/script.js
+++ event/script.js
@@ -2,9 +2,18 @@
 //     alert('Button clicked!');
 // }
 
-function hello() {
+// function hello() {
+//     console.log("Hello World");
+// }
+
+const hello = () => {
     console.log("Hello World");
 }
-
 document.getElementById('myButton').addEventListener('click', hello);
+
+// function hello() {
+//     console.log("Hello World");
+// }   
+
+
 
