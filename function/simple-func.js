34c76131062236d8e5d85ab4dd0b0a88c2170472
joynalabed5 <engr.joynal.bd@gmail.com>
2026-09-12 23:46:32 +0600

    # feature   practice arrow function

--- function/simple-func.js
+++ function/simple-func.js
@@ -12,3 +12,32 @@ ello("Joy", "Rahim");
 // arrow function with no return
 const greet = () => "Hello!";
 console.log(greet()); // Output: Hello!
+
+const green = (name) => `Hello ${name}`;
+console.log(green("Joy")); // Output: Hello Joy
+console.log(green("Rahim")); // Output: Hello Rahim
+
+//annonymous function
+document.getElementById("btn").addEventListener("click", function() {
+    console.log("Button clicked!");
+});
+
+//arrow function
+document.getElementById("btn").addEventListener("click", () => {
+    console.log("Button clicked!");
+});
+
+//callable function
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 3)); // Output: 8
+
+// callable function with addeventlistener
+document.getElementById("btn").addEventListener("click", function() {
+    console.log(add(5, 3)); // Output: 8
+}); 
+
+document.getElementById("btn").addEventListener("click", () => {
+    console.log(add(5, 3)); // Output: 8
+}   
